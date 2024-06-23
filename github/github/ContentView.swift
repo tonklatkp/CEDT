8fe30ac0e18addf9f3e6948cb395c1893957792c
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:04:20 -0700

    first

---
+++ github/github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by admin on 6/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github/github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by admin on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
